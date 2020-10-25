e81bb94cc485f9c34581b979a57515ab61a334d7
Timo de Winter <info@timodw.nl>
2020-10-25 17:10:48 +0100

    Created PWA items

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,14 +13,35 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="shortcut icon" type="image/png" href="/images/favicon.png">
+
 
         <!-- Scripts -->
         @routes
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" integrity="sha512-F5QTlBqZlvuBEs9LQPqc1iZv2UMxcVXezbHzomzS6Df4MZMClge/8+gXrKw2fl5ysdk4rWjR0vKS7NNkfymaBQ==" crossorigin="anonymous"></script>
         <script src="{{ mix('js/app.js') }}" defer></script>
+        <link rel="manifest" href="/manifest.json">
+        <meta name="theme-color" content="#6875f5">
+        <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png">
     </head>
     <body class="font-sans antialiased">
         @inertia
+
+        <noscript>
+            <div style="padding: 10px">
+                <span style="color:red">Javascript is not enabled.</span>
+            </div>
+        </noscript>
+
+        <script>
+            console.log(window.isSecureContext);
+            if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                    navigator.serviceWorker.register('/service-worker.js');
+                });
+            }
+        </script>
+
     </body>
 </html>
